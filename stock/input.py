8bdd67e59c8e9fa672433f48ad76c1f07d7ca8e5
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2017-08-16 00:03:32 +0200

    handling wrong input event type

--- stock/input.py
+++ stock/input.py
@@ -1,8 +1,16 @@
 import json
 
+SUPPORTED_TYPES = ['ProductCreated', 'ProductUpdated', 'ProductEnded']
+
+
 class Input:
     def __init__(self, event_source):
         event_source.add(self)
 
     def translate(self, event):
-        return json.loads(event)
+        translated_event = json.loads(event)
+        self.check_type(translated_event)
+        return translated_event
+
+    def check_type(self, event):
+        return SUPPORTED_TYPES.index(event['type'])
--- test/test_input.py
+++ test/test_input.py
@@ -3,6 +3,7 @@ from stock.dispatcher import Dispatcher
 from stock.input import Input
 
 CORRECT_EVENT = '{"type": "ProductCreated", "id": 1, "stock": 10, "timestamp": 123, "parent_id": null}'
+WRONG_TYPE_EVENT = '{"type": "ProductCreatedddd", "id": 1, "stock": 10, "timestamp": 123, "parent_id": null}'
 
 
 class InputTest(unittest.TestCase):
@@ -23,6 +24,9 @@ class InputTest(unittest.TestCase):
             'parent_id': None
         }
         self.assertDictEqual(translated_event, expected_event)
+
+    def test_translate_wrong_type_event(self):
+        self.assertRaises(ValueError, self.input.translate, WRONG_TYPE_EVENT)
 
 
 if __name__ == '__main__':
