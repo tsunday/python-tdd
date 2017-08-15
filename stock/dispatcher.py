26d295d93aa35fb2457eac8e0ae5f686a97b6152
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2017-08-15 22:56:51 +0200

    dispatcher observers shouldnt be static

--- stock/dispatcher.py
+++ stock/dispatcher.py
@@ -1,5 +1,10 @@
 class Dispatcher:
-    _observers = []
-
+    def __init__(self):
+        self._observers = []
+        
     def add(self, observer):
         self._observers.append(observer)
+
+    def notify(self):
+        for observer in self._observers:
+            observer.update()
--- test/test_dispatcher.py
+++ test/test_dispatcher.py
@@ -3,15 +3,27 @@ from stock.dispatcher import Dispatcher
 
 
 class Observer:
+    def __init__(self):
+        self.update_counter = 0
+
     def update(self):
+        self.update_counter += 1
         print('callback called')
 
 
 class DispatcherTest(unittest.TestCase):
+    def setUp(self):
+        self.dispatcher = Dispatcher()
+
     def test_add_observer(self):
-        dispatcher = Dispatcher()
-        dispatcher.add(Observer())
-        self.assertEqual(len(dispatcher._observers), 1)
+        self.dispatcher.add(Observer())
+        self.assertEqual(len(self.dispatcher._observers), 1)
+
+    def test_notify_observer(self):
+        observer = Observer()
+        self.dispatcher.add(observer)
+        self.dispatcher.notify()
+        self.assertEqual(observer.update_counter, 1)
 
 
 if __name__ == '__main__':
