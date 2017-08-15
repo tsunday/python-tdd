bcb5666df9e01cc467a70a1d934fe4eea07c6224
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2017-08-15 23:43:02 +0200

    input controller understands correct events

--- stock/input.py
+++ stock/input.py
@@ -1,3 +1,8 @@
+import json
+
 class Input:
     def __init__(self, event_source):
         event_source.add(self)
+
+    def translate(self, event):
+        return json.loads(event)
--- test/test_input.py
+++ test/test_input.py
@@ -2,12 +2,27 @@ import unittest
 from stock.dispatcher import Dispatcher
 from stock.input import Input
 
+CORRECT_EVENT = '{"type": "ProductCreated", "id": 1, "stock": 10, "timestamp": 123, "parent_id": null}'
+
 
 class InputTest(unittest.TestCase):
-    def test_requires_event_source(self):
-        event_source = Dispatcher()
-        input = Input(event_source)
-        self.assertEqual(len(event_source._observers), 1)
+    def setUp(self):
+        self.event_source = Dispatcher()
+        self.input = Input(self.event_source)
+
+    def test_require_event_source(self):
+        self.assertEqual(len(self.event_source._observers), 1)
+
+    def test_translate_correct_events(self):
+        translated_event = self.input.translate(CORRECT_EVENT)
+        expected_event = {
+            'type': 'ProductCreated',
+            'id': 1,
+            'stock': 10,
+            'timestamp': 123,
+            'parent_id': None
+        }
+        self.assertDictEqual(translated_event, expected_event)
 
 
 if __name__ == '__main__':
